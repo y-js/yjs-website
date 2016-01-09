31d0fcce9883e763ae9b1a8e07f7a5ca7e6a01ee
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2016-01-09 19:17:03 +0100

    updated vulcanize

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,6 @@ var glob = require('glob')
 var historyApiFallback = require('connect-history-api-fallback')
 var packageJson = require('./package.json')
 var crypto = require('crypto')
-var polybuild = require('polybuild')
 
 var AUTOPREFIXER_BROWSERS = [
   'ie >= 10',
@@ -183,7 +182,12 @@ gulp.task('html', function () {
 // scripts and CSS for you.
 gulp.task('vulcanize', function () {
   return gulp.src('dist/index.html')
-    .pipe(polybuild({maximumCrush: true}))
+    // .pipe(polybuild({maximumCrush: true}))
+    .pipe($.vulcanize({
+      stripComments: true,
+      inlineCss: true,
+      inlineScripts: true
+    }))
     .pipe(gulp.dest('dist/'))
 })
 
